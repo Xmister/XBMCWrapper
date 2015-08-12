dd0ae7e23c17389081ca4d838f4f9a45c52b075b
Xmister <xmisterhu@gmail.com>
2015-08-12 17:52:46 +0200

    Replace DAV->HTTP

--- app/src/main/java/hu/xmister/xbmcwrapper/PlayerView.java
+++ app/src/main/java/hu/xmister/xbmcwrapper/PlayerView.java
@@ -152,8 +152,9 @@ public class PlayerView extends android.support.v4.app.FragmentActivity {
 				else if (FileSmb.startsWith("dav://")) {
 					Intent LaunchIntent = new Intent(Intent.ACTION_VIEW);
 					String pkg=sharedPreferences.getString("http", "system");
-					setStatus("Launching " + pkg + " with local file...");
+					setStatus("Launching " + pkg + " with DAV->HTTP URL...");
 					if (!pkg.equals("system")) LaunchIntent.setPackage(pkg);
+					FileSmb = FileSmb.replaceFirst("(?i)dav://", "http://");
 					LaunchIntent.setDataAndType(Uri.parse(FileSmb), "video/*");
 					startActivityForResult(LaunchIntent, 1);
 				}
